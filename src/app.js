1ebb14857fd094fa5a1a9518baa69daacc273111
chethtrayen <chethtrayen1991@gmail.com>
2019-03-09 18:35:17 -0500

    Add bodyParser

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const gitRoutes = require('./git/routes');
 
+app.use(bodyParser.json());
 app.use('/git', gitRoutes);
 
 module.exports = app;
