6b3934f77ebd9519d579b87af98d27de0bbe538f
chethtrayen <chethtrayen1991@gmail.com>
2019-03-09 19:21:22 -0500

    add urlencoded

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const gitRoutes = require('./git/routes');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded())
 app.use('/git', gitRoutes);
 
 module.exports = app;
